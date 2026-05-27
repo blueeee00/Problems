56e24a985871aac5df4093d542d233df88990065
goine <blueeee.6767@gmail.com>
2026-05-27 18:26:12 +0700

    problems

---
+++ Codeforces/contest06/a.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define int long long
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define space ' '
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define mp(x, y) make_pair(x, y)
+#define pb push_back
+#define ppb pop_back
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF LLONG_MAX
+
+#define in(...) [&](auto&... args){ (cin >> ... >> args); }(__VA_ARGS__)
+#define out(...) [&](auto&&... args){ int n_ = 0; ((cout << (n_++ ? " " : "") << args), ...); cout << endl; }(__VA_ARGS__)
+#define vin(v) for (auto &x : v) cin >> x
+#define vout(v) for (auto &x : v) cout << x << space; cout << endl
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    int n;
+    in(n);
+
+    vector<int> v(n);
+    vin(v);
+
+    int mn = 1e9;
+    int mx = 0;
+    rep(i, 0, n) {
+        mn = min(mn, v[i]);
+        mx = max(mx, v[i]);
+    }
+
+    out(ceil((double) abs(mn - mx) / 2));
+}
+
+signed main() {
+    fastIO;
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
---
+++ Codeforces/contest06/c1.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define int long long
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define space ' '
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define mp(x, y) make_pair(x, y)
+#define pb push_back
+#define ppb pop_back
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF LLONG_MAX
+
+#define in(...) [&](auto&... args){ (cin >> ... >> args); }(__VA_ARGS__)
+#define out(...) [&](auto&&... args){ int n_ = 0; ((cout << (n_++ ? " " : "") << args), ...); cout << endl; }(__VA_ARGS__)
+#define vin(v) for (auto &x : v) cin >> x
+#define vout(v) for (auto &x : v) cout << x << space; cout << endl
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    int n;
+    in(n);
+
+    vector<int> a(n);
+    vin(a);
+
+    vector<int> ans;
+    int cnt = 0;
+    rep(i, n, 0) {
+        int cur = (cnt % 2 == 0) ? a[i] : -a[i];
+
+        if (cur > 0) {
+            ans.push_back(i + 1);
+            cnt++;
+        }
+    }
+
+    out(sz(ans));
+    rep(i, 0, sz(ans)) {
+        cout << ans[i] << space;
+    }
+
+    out();
+}
+
+signed main() {
+    fastIO;
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
