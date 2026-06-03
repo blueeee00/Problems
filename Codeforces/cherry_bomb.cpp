ad42591a3c556494afb82644aa09b33972bc3705
goine <blueeee.6767@gmail.com>
2026-06-03 19:29:10 +0700

    chromakopia and stuff

---
+++ Codeforces/cherry_bomb.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define int long long
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define space ' '
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define mp(x, y) make_pair(x, y)
+#define pb push_back
+#define ppb pop_back
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF LLONG_MAX
+
+#define in(...) [&](auto&... args){ (cin >> ... >> args); }(__VA_ARGS__)
+#define out(...) [&](auto&&... args){ int n_ = 0; ((cout << (n_++ ? " " : "") << args), ...); cout << endl; }(__VA_ARGS__)
+#define vin(v) for (auto &x : v) cin >> x
+#define vout(v) for (auto &x : v) cout << x << space; cout << endl
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int, string, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+
+void solve() {
+    int n, k;
+    in(n, k);
+
+    vector<int> v1(n);
+    vector<int> v2(n);
+
+    vin(v1);
+    vin(v2);
+
+    int cur = -1;
+    int mn = 1e9;
+    int mx = 0;
+    rep(i, 0, n) {
+        mn = min(mn, v1[i]);
+        mx = max(mx, v1[i]);
+
+        if (v2[i] != -1) {
+            if (cur == -1) {
+                cur = v1[i] + v2[i];
+            } else if (cur != v1[i] + v2[i]) {
+                out(0);
+                return;
+            }
+        }
+    }
+    
+    if (cur == -1) {
+        int l = mx;
+        int h = mn + k;
+        
+        if (l <= h) {
+            out(h - l + 1);
+        } else {
+            out(0);
+        }
+    } else {
+        rep(i, 0, n) {
+            if (v1[i] > cur) {
+                out(0);
+                return;
+            }
+
+            if (cur - v1[i] > k) {
+                out(0);
+                return;
+            }
+        }
+
+        out(1);
+    }
+}
+
+signed main() {
+    fastIO;
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
---
+++ Codeforces/multiplyby2_divideby6.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define int long long
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define space ' '
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define mp(x, y) make_pair(x, y)
+#define pb push_back
+#define ppb pop_back
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF LLONG_MAX
+
+#define in(...) [&](auto&... args){ (cin >> ... >> args); }(__VA_ARGS__)
+#define out(...) [&](auto&&... args){ int n_ = 0; ((cout << (n_++ ? " " : "") << args), ...); cout << endl; }(__VA_ARGS__)
+#define vin(v) for (auto &x : v) cin >> x
+#define vout(v) for (auto &x : v) cout << x << space; cout << endl
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int, string, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+
+void solve() {
+    int n;
+    in(n);
+
+    if (n == 1) {
+        out(0);
+        return;
+    }
+
+    int ans = 0;
+    int a = 0;
+    int b = 0;
+
+    while (n % 3 == 0) {
+        a++;
+        n /= 3;
+    }
+
+    while (n % 2 == 0) {
+        b++;
+        n /= 2;
+    }
+
+    if (n != 1) {
+        out(-1);
+        return;
+    }
+
+    if (b > a) {
+        out(-1);
+        return;
+    } else {
+        out(a + (a - b));
+    }
+}
+
+signed main() {
+    fastIO;
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
